9922ff3e3475d4f37557528ec559cd095ff4feab
bayupw <bayupw@gmail.com>
2022-04-27 10:36:08 +1200

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,15 @@
-output "gcp_syd_transit03" {
-  description = "gcp_syd_transit03"
-  value       = module.gcp_syd_transit03
-  sensitive   = false
+output "aviatrix_transit_gateway" {
+  description = "aviatrix_transit_gateway"
+  value       = module.gcp_syd_transit03.aviatrix_transit_gateway
 }
 
-output "gcp_syd_fortigate" {
+output "gcp_syd_transit03_aviatrix_vpc" {
+  description = "aviatrix_vpc"
+  value       = module.gcp_syd_transit03.aviatrix_vpc
+}
+
+output "gcp_syd_fortigate_aviatrix_vpc" {
   description = "gcp_syd_fortigate"
-  value       = module.gcp_syd_fortigate
-  sensitive   = false
-}
-
-output "zones" {
-  description = "zones"
-  value       = data.google_compute_zones.available
+  value       = module.gcp_syd_fortigate.aviatrix_vpc
   sensitive   = false
 }
